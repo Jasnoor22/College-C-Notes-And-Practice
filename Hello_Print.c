679e541e3db3dd104c4993afa14e96e65853948e
Jasnoor Singh <81462656+Jasnoor22@users.noreply.github.com>
2025-08-06 21:06:55 +0530

    Add files via upload

---
+++ Hello_Print.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+//lecture 1 basics
+int main () {
+    int num1, num2, result;
+    printf("Enter Num1 and Num2 :");
+    scanf("%d %d", &num1, &num2);
+    result = num1 + num2;
+    printf("Sum of the numbers = %d\n", result);
+    result = num1 - num2;
+    printf("Difference of the numbers = %d\n", result);
+    result = num1 * num2;
+    printf("Product of the numbers = %d\n", result);
+    result = num1 / num2;
+    printf("Division of the numbers = %f\n", result);
+    return 0;
+}
